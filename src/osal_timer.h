78277313534462cb1f258253f39be7cb681ff6a7
lelanthran <lelanthran@gmail.com>
2023-08-06 22:52:04 +0200

    Adds osal_timer, adapted from xtimer.

--- src/osal_timer.h
+++ src/osal_timer.h
@@ -2,16 +2,51 @@
 #ifndef H_OSAL_TIMER
 #define H_OSAL_TIMER
 
+
+typedef struct osal_timer_t osal_timer_t;
+
+// Convenience macros to convert to/from microseconds
+#define osal_timer_convert_us_to_s(x)\
+   (double)((double)x / 1000000.0)
+#define osal_timer_convert_us_to_ms(x)\
+   (double)((double)x * 1000.0)
+#define osal_timer_convert_s_to_us(x)\
+   (uint64_t)((uint64_t)x * (uint64_t)1000000ULL)
+#define osal_timer_convert_ms_to_us(x)\
+   (uint64_t)((uint64_t)x * (uint64_t)1000ULL)
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+   // Initialise all the timer structures and values.
+   void osal_timer_init (void);
 
+   // Returns the number of microseconds that have elapsed since this
+   // function was last called. On the first invocation it returns
+   // the number of microseconds since osal_timer_init() was called.
+   uint64_t osal_timer_mark_us (void);
+
+   // Returns the number of microseconds since the program init().
+   uint64_t osal_timer_since_start (void);
+
+   // Sets a timer for expiry in the future, in ns. The caller must
+   // use osal_timer_expired() to determine if the timer has expired.
+   // Use reset() for reusing an existing timer, or set() for
+   // allocating a new timer
+   osal_timer_t *osal_timer_set (uint64_t micros);
+   bool osal_timer_reset (osal_timer_t *xt, uint64_t micros);
+
+   // Check if a timer expired. Timer must have been set with
+   // osal_timer_set() above.
+   bool osal_timer_expired (osal_timer_t *xt);
+
+   // Cancel and delete a timer.
+   void osal_timer_del (osal_timer_t *xt);
 
 #ifdef __cplusplus
 };
 #endif
-
 
 #endif
 
--- src/test_timer.c
+++ src/test_timer.c
@@ -1,10 +1,75 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "osal_timer.h"
+
+void spinwait (uint64_t us)
+{
+   osal_timer_t *tmp = NULL;
+
+   tmp = osal_timer_set (us);
+   while (!osal_timer_expired (tmp))
+      ;
+
+   if (tmp) {
+      osal_timer_del (tmp);
+   }
+}
 
 int main (void)
 {
-   int ret = EXIT_FAILURE;
-   return ret;
-}
+   const char paddle[] = "-\\|/";
+   uint64_t mark = 0;
 
+   printf ("osal v%s: Testing osal_timer functionality\n", osal_version);
+   osal_timer_init ();
+
+   printf ("starting at: %" PRIu64 "\n ", osal_timer_since_start ());
+   osal_timer_mark_us ();
+   for (size_t i=0; i<1024; i++) {
+      printf ("\b%c", paddle[i % (sizeof paddle)]);
+   }
+
+   mark = osal_timer_mark_us ();
+   printf ("\nLoop duration:  %" PRIu64 "uS (%.2fs)\n", mark, osal_timer_convert_us_to_s (mark));
+   printf ("ending at: %" PRIu64 " \n", osal_timer_since_start ());
+
+
+   printf ("Starting expired test (15 seconds)\n ");
+   osal_timer_t *t1 = osal_timer_set (osal_timer_convert_s_to_us (15));
+   printf ("Mark at: %" PRIu64 " \n", osal_timer_since_start ());
+   osal_timer_mark_us ();
+   while (!osal_timer_expired (t1)) {
+      //printf ("\b%c", paddle[i++ % (sizeof paddle)]);
+      putchar ('.');
+      fflush (stdout);
+      spinwait (osal_timer_convert_ms_to_us (250));
+   }
+   mark = osal_timer_mark_us ();
+   printf ("\nLoop duration:  %" PRIu64 "uS (%.2fs)\n", mark, osal_timer_convert_us_to_s (mark));
+
+   printf ("Testing resets (reset timer for 3 seconds)\n");
+   printf ("Mark at: %" PRIu64 " \n", osal_timer_since_start ());
+
+   osal_timer_reset (t1, osal_timer_convert_s_to_us (3));
+
+   printf ("Mark at: %" PRIu64 " \n", osal_timer_since_start ());
+   osal_timer_mark_us ();
+   while (!osal_timer_expired (t1)) {
+      // printf ("\b%c", paddle[i++ % (sizeof paddle)]);
+      putchar ('.');
+      fflush (stdout);
+      spinwait (osal_timer_convert_ms_to_us (250));
+   }
+   mark = osal_timer_mark_us ();
+   printf ("\nLoop duration:  %" PRIu64 "uS (%.2fs)\n", mark, osal_timer_convert_us_to_s (mark));
+   printf ("Mark at: %" PRIu64 " \n", osal_timer_since_start ());
+
+   osal_timer_del (t1);
+   return EXIT_SUCCESS;
+}
